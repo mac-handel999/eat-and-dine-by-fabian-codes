b752cb57243cf0929682ee1219c7da01bd5d3e7f
mac-handel999 <fabikechukwuemeana@gmail.com>
2024-09-23 10:39:05 +0100

    update

---
+++ main.js
@@ -0,0 +1,9 @@
+const foodName = document. querySelector('.food-name');
+
+const dayTime = document.querySelector('.time-of-day');
+
+const ingredients = document. querySelector('.bf-ingre');
+
+const btn = document.querySelector('.get-recipe');
+
+const url = 
